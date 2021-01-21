113c2df0261a62c84ac7863b52153531fd39b547
87cunzhang <2423561050@qq.com>
2021-01-21 20:19:25 +0800

    test

--- parser.go
+++ parser.go
@@ -16,7 +16,7 @@ var Confs map[string]config.Configer
 func init() {
 	Confs = make(map[string]config.Configer)
 	// 读取conf下所有文件.
-	files, err := ioutil.ReadDir("../conf")
+	files, err := ioutil.ReadDir(".../conf")
 	if err != nil {
 		log.Fatal(err)
 	}
